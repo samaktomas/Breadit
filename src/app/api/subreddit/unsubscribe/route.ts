c373fa47f6cca1262ae08f567ed2e57aa343e773
Tomas Samak <samaktomas@gmail.com>
2023-07-07 16:45:29 +0200

    init commit

---
+++ src/app/api/subreddit/unsubscribe/route.ts
@@ -0,0 +1,65 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = req.json();
+
+    const { subredditId } = SubredditSubscriptionValidator.parse(body);
+
+    const subscriptionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+
+    if (!subscriptionExists) {
+      return new Response(
+        "You've not been subscribed to this subreddit, yet.",
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const subreddit = await db.subreddit.findFirst({
+      where: {
+        id: subredditId,
+        creatorId: session.user.id,
+      },
+    });
+
+    if (subreddit) {
+      return new Response("You can't unsubscribe form your own subreddit", {
+        status: 400,
+      });
+    }
+
+    await db.subscription.delete({
+      where: {
+        userId_subredditId: {
+          subredditId,
+          userId: session.user.id,
+        },
+      },
+    });
+
+    return new Response(subredditId);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid requestdata  passed", { status: 422 });
+    }
+    return new Response("Could not subscribe, please try again later", {
+      status: 500,
+    });
+  }
+}
---
+++ src/components/CustomFeed.tsx
@@ -0,0 +1,45 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { db } from "@/lib/db";
+import React from "react";
+import PostFeed from "./PostFeed";
+import { getAuthSession } from "@/lib/auth";
+import { notFound } from "next/navigation";
+
+async function CustomFeed() {
+  const session = await getAuthSession();
+
+  if (!session) return notFound();
+
+  const followedCommunities = await db.subscription.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      subreddit: true,
+    },
+  });
+
+  const posts = await db.post.findMany({
+    where: {
+      subreddit: {
+        name: {
+          in: followedCommunities.map((sub) => sub.subreddit.name),
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      subreddit: true,
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+  });
+
+  return <PostFeed initialPosts={posts} />;
+}
+
+export default CustomFeed;
